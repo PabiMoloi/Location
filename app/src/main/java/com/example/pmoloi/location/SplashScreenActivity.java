583c55302799626b48392fcbb3b19a5b08a05707
Paballo Moloi <PMoloi@jhb.dvt.co.za>
2018-02-02 17:47:57 +0200

    Splash screen addition

--- app/src/main/java/com/example/pmoloi/location/SplashScreenActivity.java
+++ app/src/main/java/com/example/pmoloi/location/SplashScreenActivity.java
@@ -2,12 +2,17 @@ package com.example.pmoloi.location;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SplashScreenActivity extends AppCompatActivity {
+
+    private TextView appVersionCode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        appVersionCode = findViewById(R.id.textViewSplashScreenVersionCodeLabel);
+        appVersionCode.setText(BuildConfig.VERSION_NAME);
     }
 }
---
+++ app/src/main/java/com/example/pmoloi/location/model/LocationModel.java
@@ -0,0 +1,8 @@
+package com.example.pmoloi.location.model;
+
+/**
+ * Created by PMoloi on 2018/02/02.
+ */
+
+public class LocationModel {
+}
