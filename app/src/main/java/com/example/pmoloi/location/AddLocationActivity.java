ceeda4dbb31ecbb4599097f78cf53d7a2c6c7910
Paballo Moloi <PMoloi@jhb.dvt.co.za>
2018-02-05 13:18:30 +0200

    Map data collection

--- app/src/main/java/com/example/pmoloi/location/AddLocationActivity.java
+++ app/src/main/java/com/example/pmoloi/location/AddLocationActivity.java
@@ -1,13 +1,26 @@
 package com.example.pmoloi.location;
 
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.pmoloi.location.ViewModel.LocationViewModel;
+import com.example.pmoloi.location.model.LocationModel;
+
 public class AddLocationActivity extends AppCompatActivity {
+
+    private TextInputLayout locationName, locationBestFeature, locationType,locationNumberOfVisits;
+    private int mNumberOfVisits;
+    LocationViewModel locationViewModel;
+    private Double mapLocationLatitude,mapLocationLongitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_location);
+
+        mapLocationLatitude = getIntent().getDoubleExtra("mMapCoordinatesLatitude",0);
+        mapLocationLongitude = getIntent().getDoubleExtra("mMapCoordinatesLongitude",0);
+        locationViewModel = new LocationViewModel(this.getApplication());
     }
 }
